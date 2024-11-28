d0ec1d2360accb3bfef31513445812d2346bf7a1
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-28 10:47:18 +0530

    commit

--- LinkedList_ll.java/CycleLinkedList.java
+++ LinkedList_ll.java/CycleLinkedList.java
@@ -17,7 +17,7 @@ public class CycleLinkedList {
     Node slow=head;
     Node fast=head;
 
-    while(fast!=null || fast.next!=null){
+    while(fast!=null && fast.next!=null){
         slow=slow.next;
         fast=fast.next.next;
         if(slow==fast){
