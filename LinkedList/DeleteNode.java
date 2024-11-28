00f9174cfa46e64b0fca2394ebdf53151ec522ce
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-28 09:29:14 +0530

    commit

--- LinkedList/DeleteNode.java
+++ LinkedList/DeleteNode.java
@@ -19,8 +19,22 @@ public class DeleteNode{
         }
         System.out.println("null");
     }
-    public void deleteNodes()
+    public static void deleteNodes(Node node) {
+        if (node != null && node.next != null) {
+            node.data = node.next.data;
+            node.next = node.next.next;
+        }
+    }
+
     public static void main(String[] args) {
-        
+        head = new Node(4);
+        head.next = new Node(5);
+        head.next.next = new Node(1);
+        head.next.next.next = new Node(9);
+
+        DeleteNode list = new DeleteNode();
+        Node nodeToDelete = head.next; // Node with value 5
+        deleteNodes(nodeToDelete);
+        list.print();
     }
 }
