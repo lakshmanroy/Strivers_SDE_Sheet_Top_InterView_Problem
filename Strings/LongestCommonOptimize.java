467b7dc79572fa756c372f32230aa86415bc005a
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2025-01-04 21:58:47 +0530

    commit

---
+++ Strings/LongestCommonOptimize.java
@@ -0,0 +1,22 @@
+public class LongestCommonOptimize {
+    public static String logestCommonPrefix(String str[]){
+        if(str==null || str.length==0){
+            return "";
+        }
+        String prefix= str[0];
+        for(int i=1;i<str.length;i++){
+            while(str[i].indexOf(prefix)!=0){
+                prefix=prefix.substring(0,prefix.length()-1);
+                if(prefix.isEmpty()){
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+    public static void main(String[] args) {
+        String str[]={"flower","flow","flight"};
+
+        System.out.println(logestCommonPrefix(str));
+    }
+}
